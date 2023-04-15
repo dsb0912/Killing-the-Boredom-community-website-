63776a62364bdd00f1d2d7fb421e3f6166f7ecf1
Mikhiel39 <100682873+Mikhiel39@users.noreply.github.com>
2023-04-15 15:40:53 +0530

    Home page with quiz

---
+++ Home page with quiz/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "During the First World War, which country signed the Peace Treaty (1917) with Germany ?",
+    answer: "2) Russia",
+    options: [
+      "1) England",
+      "2) Russia",
+      "3) USA",
+      "4) Austria"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When did United Kingdom declared war on Germany which lead to World War II ?",
+    answer: "4) 3rd September 1939",
+    options: [
+      "1) 4th April 1939",
+      "2) 18th August 1939",
+      "3) 28th August 1939",
+      "4) 3rd September 1939"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In which year, America joined the Second World War ?",
+    answer: "1) 1941",
+    options: [
+      "1) 1941",
+      "2) 1939",
+      "3) 1940",
+      "4) 1942"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The immediate cause for the outbreak of the first World War was ?",
+    answer: "4) The assassination of Archduke Francis Ferdinand",
+    options: [
+      "1) The imprisonment of Lenin",
+      "2) The ambition of America to dominate the world",
+      "3) The sudden death of Lloyd George",
+      "4) The assassination of Archduke Francis Ferdinand"
+    ]
+  },
+    {
+    numb: 5,
+    question: "On which side did Japan fight in the First World War ?",
+    answer: "2) With United Kingdom against Germany",
+    options: [
+      "1) With Germany against United Kingdom",
+      "2) With United Kingdom against Germany",
+      "3) Against Russia on its own",
+      "4) None, it was neutral"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
